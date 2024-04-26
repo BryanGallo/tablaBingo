6bb1b65fe970591932b58bf8964bc7fc27b0a223
Bryan Gallo <bryangallo95@hotmail.com>
2024-04-25 23:55:12 -0500

    Bolas del 1 al 75 colocadas

--- src/data/bolas.js
+++ src/data/bolas.js
@@ -73,6 +73,50 @@ const bolasLetras = [
         letra: "I",
     },
     {
+        numero: 19,
+        letra: "I",
+    },
+    {
+        numero: 20,
+        letra: "I",
+    },
+    {
+        numero: 21,
+        letra: "I",
+    },
+    {
+        numero: 22,
+        letra: "I",
+    },
+    {
+        numero: 23,
+        letra: "I",
+    },
+    {
+        numero: 24,
+        letra: "I",
+    },
+    {
+        numero: 25,
+        letra: "I",
+    },
+    {
+        numero: 26,
+        letra: "I",
+    },
+    {
+        numero: 27,
+        letra: "I",
+    },
+    {
+        numero: 28,
+        letra: "I",
+    },
+    {
+        numero: 29,
+        letra: "I",
+    },
+    {
         numero: 30,
         letra: "I",
     },
@@ -86,6 +130,50 @@ const bolasLetras = [
     },
     {
         numero: 33,
+        letra: "N",
+    },
+    {
+        numero: 34,
+        letra: "N",
+    },
+    {
+        numero: 35,
+        letra: "N",
+    },
+    {
+        numero: 36,
+        letra: "N",
+    },
+    {
+        numero: 37,
+        letra: "N",
+    },
+    {
+        numero: 38,
+        letra: "N",
+    },
+    {
+        numero: 39,
+        letra: "N",
+    },
+    {
+        numero: 40,
+        letra: "N",
+    },
+    {
+        numero: 41,
+        letra: "N",
+    },
+    {
+        numero: 42,
+        letra: "N",
+    },
+    {
+        numero: 43,
+        letra: "N",
+    },
+    {
+        numero: 44,
         letra: "N",
     },
     {
@@ -105,6 +193,50 @@ const bolasLetras = [
         letra: "G",
     },
     {
+        numero: 49,
+        letra: "G",
+    },
+    {
+        numero: 50,
+        letra: "G",
+    },
+    {
+        numero: 51,
+        letra: "G",
+    },
+    {
+        numero: 52,
+        letra: "G",
+    },
+    {
+        numero: 53,
+        letra: "G",
+    },
+    {
+        numero: 54,
+        letra: "G",
+    },
+    {
+        numero: 55,
+        letra: "G",
+    },
+    {
+        numero: 56,
+        letra: "G",
+    },
+    {
+        numero: 57,
+        letra: "G",
+    },
+    {
+        numero: 58,
+        letra: "G",
+    },
+    {
+        numero: 59,
+        letra: "G",
+    },
+    {
         numero: 60,
         letra: "G",
     },
@@ -118,6 +250,50 @@ const bolasLetras = [
     },
     {
         numero: 63,
+        letra: "O",
+    },
+    {
+        numero: 64,
+        letra: "O",
+    },
+    {
+        numero: 65,
+        letra: "O",
+    },
+    {
+        numero: 66,
+        letra: "O",
+    },
+    {
+        numero: 67,
+        letra: "O",
+    },
+    {
+        numero: 68,
+        letra: "O",
+    },
+    {
+        numero: 69,
+        letra: "O",
+    },
+    {
+        numero: 70,
+        letra: "O",
+    },
+    {
+        numero: 71,
+        letra: "O",
+    },
+    {
+        numero: 72,
+        letra: "O",
+    },
+    {
+        numero: 73,
+        letra: "O",
+    },
+    {
+        numero: 74,
         letra: "O",
     },
     {
